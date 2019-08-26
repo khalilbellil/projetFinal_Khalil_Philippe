038aa40dfcbc1bf38751a1e8916d7ebcdfdd426a
khalilbellil <khalilbellil.ca@gmail.com>
2019-08-26 10:02:50 -0400

    dialogueManager

---
+++ projetFinalKhalilPhilippe/Assets/Scripts/Managers/DialogueManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager
+{
+
+    #region Singleton Pattern
+    private static DialogueManager instance = null;
+    private DialogueManager() { }
+    public static DialogueManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new DialogueManager();
+            }
+            return instance;
+        }
+    }
+    #endregion
+
+    
+
+}
