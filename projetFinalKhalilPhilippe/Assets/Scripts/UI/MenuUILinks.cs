258cb8da6f9b888ccde133d701d05d7a12b56336
khalilbellil <khalilbellil.ca@gmail.com>
2019-09-06 01:08:25 -0400

    Adding MainMenu Scene and Renamed Prototype Scene to Game

---
+++ projetFinalKhalilPhilippe/Assets/Scripts/UI/MenuUILinks.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuUILinks : MonoBehaviour
+{
+
+    public Button playButton;
+    public Button exitButton;
+
+}
